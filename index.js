20016642bd1567960e41c11e94979d911726cbad
fongelias <fongelias@gmail.com>
2018-01-07 12:47:20 -0500

    finished demo

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 
 const app = require('express')();
 
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+const server = app.listen(3000);
+
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', msg);
+	});
+});
 
 
 
